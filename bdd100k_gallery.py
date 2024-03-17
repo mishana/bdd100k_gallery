c26e5a1f8f5332404ea431fff458473727ce7b2b
Michael Leybovich <mishana4life@gmail.com>
2024-03-18 00:09:13 +0200

    Change page controller
    Signed-off-by: Michael Leybovich <mishana4life@gmail.com>

--- bdd100k_gallery.py
+++ bdd100k_gallery.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import streamlit as st
+from streamlit_image_comparison import image_comparison
 
 
 st.set_page_config(layout="wide")
@@ -12,7 +13,6 @@ images_ext = 'webp'
 def get_all_filtered_images_names():
     images_metadata = pd.read_csv('images_metadata.csv')
     all_images_names = [f'{f_idx}.{images_ext}' for i, f_idx in enumerate(images_metadata['frame_idx']) if not images_metadata.iloc[i]['is_filter']]
-    print(all_images_names[:10])
     return all_images_names
 
 @st.cache_data
@@ -20,7 +20,6 @@ def get_images_names_to_sorted_idx():
     images_metadata = pd.read_csv('images_metadata.csv')
     all_images_names = [f'{f_idx}.{images_ext}' for i, f_idx in enumerate(images_metadata['frame_idx'])]
     images_names_to_sorted_idx = {img_name: i for i, img_name in enumerate(sorted(all_images_names))}
-    print("HIII", sorted(all_images_names).index('3234.webp'))
     return images_names_to_sorted_idx
 
 @st.cache_data
@@ -32,10 +31,8 @@ def get_dataset():
 
 @st.cache_data
 def get_image_from_folder(data_subset, image_name):
-    # print(image_name)
     images_names_to_sorted_idx = get_images_names_to_sorted_idx()
     sorted_idx = images_names_to_sorted_idx[image_name]
-    print(sorted_idx)
 
     dataset = get_dataset()
     label_names = dataset.features['label'].names
@@ -60,6 +57,9 @@ with col_center:
 # Initialize or update the current page in the session state
 if 'current_page' not in st.session_state:
     st.session_state.current_page = 0  # Starting from page 0
+
+def enter_page_num():
+    st.session_state.current_page = st.session_state.page_num - 1
 
 def increment_page():
     st.session_state.current_page += 1
@@ -89,30 +89,42 @@ with col3:
     st.text('Automatically Relabeled Bboxes')
 
 for i in range(start_index, min(end_index, total_images)):
-    col1, col2, col3 = st.columns(3, gap='large')
+    # col1, col2, col3 = st.columns(3, gap='large')
 
     img_name = get_all_filtered_images_names()[i]
     
     img_all, label_all = get_image_from_folder('all_bboxes', img_name)
     with col1:
-        # st.image(img_all, caption=f"Label: {label_all}", use_column_width=True)
-        st.image(img_all, use_column_width=True)
+        st.image(img_all, caption=f"Index: {label_all}", use_column_width=True)
+        # st.image(img_all, use_column_width=True, caption="")
 
     img_suspects, label_suspects = get_image_from_folder('suspect_bboxes', img_name)
     with col2:
-        st.image(img_suspects, caption=f"Label: {label_suspects}", use_column_width=True)
+        st.image(img_suspects, caption=f"Index: {label_suspects}", use_column_width=True)
         # st.image(img_suspects, use_column_width=True)
 
     img_relabeled, label_relabeled = get_image_from_folder('relabeled_bboxes', img_name)
     with col3:
-        # st.image(img_relabeled, caption=f"Label: {label_relabeled}", use_column_width=True)
-        st.image(img_relabeled, use_column_width=True)
+        st.image(img_relabeled, caption=f"Index: {label_relabeled}", use_column_width=True)
+        # st.image(img_relabeled, use_column_width=True, caption=" ")
 
 # Centered navigation buttons with conditional disabling
-_, col_prev, col_next, _ = st.columns([2,1,1,2])
+_, col_prev, col_page_num, col_next, _ = st.columns([2,1,1,1,2], gap='large')
 
-with col_prev:
-    st.button('Previous', disabled=prev_disabled(), key='prev', on_click=decrement_page)
+# with col_prev:
+#     st.button('Previous', disabled=prev_disabled(), key='prev', on_click=decrement_page)
 
-with col_next:
-    st.button('Next', disabled=next_disabled(), key='next', on_click=increment_page)
+# with col_next:
+#     st.button('Next', disabled=next_disabled(), key='next', on_click=increment_page)
+
+# with col_prev:
+#     image_comparison(
+#         img_suspects,
+#         img_relabeled,
+#         label1="Suspect Bboxes",
+#         label2="Automatically Relabeled Bboxes",
+#     )
+
+with col_page_num:
+    num = st.number_input(label="Enter page num:", on_change=enter_page_num, value=1, step=1, label_visibility='visible', min_value=1, max_value=total_pages, key='page_num')
+
